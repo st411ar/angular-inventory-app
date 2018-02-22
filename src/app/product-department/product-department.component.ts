2185e44889eae0b7a51bee31484786aa53505bf3
Vitaly Kiselev <st411ar@ya.ru>
2018-02-23 02:01:08 +0300

    completed product department component, added menu header

--- src/app/product-department/product-department.component.ts
+++ src/app/product-department/product-department.component.ts
@@ -3,15 +3,8 @@ import {Product} from '../product.model';
 
 @Component({
   selector: 'app-product-department',
-  templateUrl: './product-department.component.html',
-  styleUrls: ['./product-department.component.css']
+  templateUrl: './product-department.component.html'
 })
-export class ProductDepartmentComponent implements OnInit {
+export class ProductDepartmentComponent {
   @Input() product: Product;
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
 }
