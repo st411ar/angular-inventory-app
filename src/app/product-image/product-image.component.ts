11bb2c4713bbc642419237376b68352fd0b1f25c
Vitaly Kiselev <st411ar@ya.ru>
2018-02-23 01:39:28 +0300

    fixed uri to url, completed product row template and product image component

--- src/app/product-image/product-image.component.ts
+++ src/app/product-image/product-image.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
+import {Product} from '../product.model';
 
 @Component({
   selector: 'app-product-image',
-  templateUrl: './product-image.component.html',
-  styleUrls: ['./product-image.component.css']
+  template: `
+    <img class="product-image" [src]="product.imageUrl"/>
+  `
 })
 export class ProductImageComponent implements OnInit {
+  @Input() product: Product;
+  @HostBinding('attr.class') cssClass = 'ui small image';
 
   constructor() { }
 
--- src/app/product.model.ts
+++ src/app/product.model.ts
@@ -2,7 +2,7 @@ export class Product {
   constructor(
     public sku: string,
     public name: string,
-    public imageUri: string,
+    public imageUrl: string,
     public department: string[],
     public price: number
   ) {}
